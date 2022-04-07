1f670a473cc7bd2842c6f968e31dd751e635abeb
Jennyfer C Santos <33470911+jennyarch@users.noreply.github.com>
2022-04-07 14:32:55 -0300

    modificada arquivos server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Batata');
+const express = require(' express');
+
+const app = express();
+const { resolve } = require('path')
+app.use('/', express.static(resolve(__dirname, './build')))
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+  console.log('tudo funcionando certinho!')
+})
